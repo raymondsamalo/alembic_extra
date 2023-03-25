dfc3a4fbc2c409facc26f3ff55bced7ba6d17aec
Ray Samalo <raymond.samalo@gmail.com>
2023-03-25 12:21:54 +0800

    Update ddl.py

--- ddl.py
+++ ddl.py
@@ -1,3 +1,5 @@
+import json
+import logging
 from abc import abstractmethod, ABC
 from typing import Tuple, Any, List
 
@@ -7,8 +9,12 @@ from alembic.operations import Operations, MigrateOperation
 from sqlalchemy import DDL, text
 from sqlmodel import SQLModel
 
+logger = logging.getLogger(__name__)
+
 
 class DDLHandler(ABC):
+    __schema_name__: str  # assigned by create_ddl
+    __table_name__: str  # assigned by create_ddl
     @abstractmethod
     def upgrade(self, schema_name: str, table_name: str) -> List[str]:
         """
@@ -30,12 +36,11 @@ class DDLHandler(ABC):
         pass
 
     @abstractmethod
-    def ddl_name_sql_query_from_schema(self, schema_name: str) -> str:
+    def ddl_name_sql_query(self, schema_name: str, table_name: str) -> str:
         """
         Query database to see if there is ddl_name in db
-        e.g.
-            find table_name or function_name and transform it to match our unique ddl_name
         :param schema_name:
+        :param table_name:
         :return: SQL query that return 1 ddl_name per matching row
         """
         pass
@@ -60,12 +65,14 @@ def create_ddl(model: SQLModel, ddl_handler: DDLHandler):
     table_name = model.__tablename__
     ddl_name = ddl_handler.ddl_name_from_metadata(schema_name, table_name)
     model.metadata.info.setdefault("ddl", list()).append(
-        {
-            "schema": schema_name,
-            "table": table_name,
-            "ddl_name": ddl_name
-        }
+            {
+                "schema": schema_name,
+                "table": table_name,
+                "ddl_name": ddl_name
+            }
     )
+    ddl_handler.__table_name__ = table_name
+    ddl_handler.__schema_name__ = schema_name
     ddl_registry[ddl_name] = ddl_handler
 
 
@@ -74,14 +81,14 @@ class AddDDLOp(MigrateOperation):
     def to_diff_tuple(self) -> Tuple[Any, ...]:
         pass
 
-    def __init__(self, ddl_name, schema, table_name):
-        self.table_name = table_name
+    def __init__(self, ddl_name, schema, hypertable_name):
+        self.table_name = hypertable_name
         self.schema = schema
         self.ddl_name = ddl_name
 
     @classmethod
-    def add_ddl(cls, operations, ddl_name, schema, table_name, **kw):
-        op = AddDDLOp(ddl_name, schema, table_name, **kw)
+    def add_ddl(cls, operations, ddl_name, schema, hypertable_name, **kw):
+        op = AddDDLOp(ddl_name, schema, hypertable_name, **kw)
         return operations.invoke(op)
 
     def reverse(self):
@@ -101,8 +108,8 @@ class DropDDLOp(MigrateOperation):
         self.ddl_name = ddl_name
 
     @classmethod
-    def drop_ddl(cls, operations, ddl_name, schema, table_name, **kw):
-        op = DropDDLOp(ddl_name, schema, table_name, **kw)
+    def drop_ddl(cls, operations, ddl_name, schema, hypertable_name, **kw):
+        op = DropDDLOp(ddl_name, schema, hypertable_name, **kw)
         return operations.invoke(op)
 
     def reverse(self):
@@ -150,20 +157,25 @@ def render_drop_ddl(autogen_context, op):
 
 @comparators.dispatch_for("schema")
 def compare_ddl(autogen_context, upgrade_ops, schemas):
+    logger.info('compare_ddl')
     metadata_in_database = set()
 
     for sch in schemas:
         schema_name = autogen_context.dialect.default_schema_name if sch is None else sch
         for ddl_name, ddl_handler in ddl_registry.items():
-            statement = ddl_handler.ddl_name_sql_query_from_schema(schema_name)
+            table_name = ddl_handler.__table_name__
+            statement = ddl_handler.ddl_name_sql_query(schema_name, table_name)
             result = autogen_context.connection.execute(text(statement))
-            metadata_in_database.update([(ddl_name, sch, row[0]) for row in result])
+            if ddl_handler.__schema_name__ is not None:
+                metadata_in_database.update([(row[0], schema_name, table_name) for row in result])
+            else:
+                metadata_in_database.update([(row[0], None, table_name) for row in result])
 
     metadata_table_list = autogen_context.metadata.info.setdefault(
         "ddl", list())
 
     metadata_hypertable = set([(m["ddl_name"], m["schema"], m["table"]) for m in metadata_table_list])
-
+  
     # for new names, produce CreateSequenceOp directives
     for ddl_name, sch, table in metadata_hypertable.difference(metadata_in_database):
         upgrade_ops.ops.append(
